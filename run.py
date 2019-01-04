2845e2541c911864b82c845f4a08c9896ad634ea
Rob Seidman <RobertDSeidman@gmail.com>
2019-01-04 14:40:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ host='0.0.0.0'
 
 # I couldn't add the services to this instance of the app so VCAP is empty
 # do this to workaround for now
-'''
+
 if 'VCAP_SERVICES' in os.environ:
     if str(os.environ['VCAP_SERVICES']) == '{}':
         print ('Using a file to populate VCAP_SERVICES')
@@ -50,7 +50,7 @@ if 'RUN_LOCAL' in os.environ:
     with open('VCAP.json') as data_file:
         data = json.load(data_file)
     os.environ['VCAP_SERVICES'] = json.dumps(data)
-'''
+
 #======================================MAIN PAGES======================================
 @app.route('/')
 def run():
